452e1f18230627fb4d88c8033658c5458651f29b
alex <osenchyn@gmail.com>
2022-09-14 15:13:34 +0300

    added MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const products = require('./data/products');
-const dotenv = require('dotenv');
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config()
+
 const app = express()
 
+connectDB()
 
 app.get('/', (req, res) => {
     res.send('API is running')
