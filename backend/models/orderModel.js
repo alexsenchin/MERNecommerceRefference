73cfd14812091d0fb45dcc1d7ec6053f5375026e
alex <osenchyn@gmail.com>
2022-09-15 19:13:49 +0300

    added order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -20,20 +20,58 @@ const orderSchema = mongoose.Schema({
         }
     ],
     shippingAdress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true }
+    },
+    paymentMethod: {
         type: String,
         required: true
     },
-    isAdmin: {
+    paymentResult: {
+        id: { type: String },
+        status: { type: String },
+        update_time: { type: String },
+        email_adress: { type: String }
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    isPaid: {
         type: Boolean,
         required: true,
         default: false
+    },
+    paidAt: {
+        type: Date
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    deliveredAt: {
+        type: Date
     }
 }, {
     timestamps: true
 })
 
 
-const User = mongoose.model('User', userSchema)
+const Order = mongoose.model('Order', orderSchema)
 
 
-export default User
+export default Order
