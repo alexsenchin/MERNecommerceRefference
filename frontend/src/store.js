b974f7451148c978ca5fccd807fd6443bd4eb325
alex <osenchyn@gmail.com>
2022-09-16 15:00:17 +0300

    added some reducers

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { combineReducers, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers.js';
+
+
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+const initialState = {}
+const middleware = [thunk]
+const store = configureStore({ reducer, initialState}, composeWithDevTools(applyMiddleware(...middleware)))
+
+
+export default store
